3be3d5bf6cd8a876e9931834cb43f08f33becbd1
Jon Montgomery <jonmontgomery@IBLT-1368.local>
2026-02-26 14:43:03 -0700

    update imports

--- src/docusearch/__init__.py
+++ src/docusearch/__init__.py
@@ -5,8 +5,9 @@ Core package for document search utilities.
 """
 
 from .docusearch import Docusearch
+from .query import RateLimitManager
 from .types import EmbeddingClient, QueryModel
 
-__all__ = ["Docusearch", "EmbeddingClient", "QueryModel", "__version__"]
+__all__ = ["Docusearch", "EmbeddingClient", "QueryModel", "RateLimitManager", "__version__"]
 
 __version__ = "0.1.0"
